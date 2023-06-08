5eb3e6cabbfac6b4423cddf95f1c9fe280c16b34
Clara Sanson <btsherwatching@gmail.com>
2023-06-08 15:32:00 -0300

    perfil config finalizado e correção de erros

--- app/src/main/java/com/example/spacebar/perfil_config.java
+++ app/src/main/java/com/example/spacebar/perfil_config.java
@@ -9,12 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -37,10 +32,10 @@ import java.sql.SQLException;
 public class perfil_config extends AppCompatActivity {
 
     private ImageView imageView, imgFundo, imgSelo1, imgSelo2;
-    private ActivityResultLauncher<Intent> galleryLauncher;
+
     private Button btnSalvar;
     private EditText txtNome, txtBio;
-    private boolean isPerfilButtonPressed = false;
+    private boolean isPerfilButtonPressed = false, isFundoButtonPressed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -134,17 +129,41 @@ public class perfil_config extends AppCompatActivity {
             ex.printStackTrace();
         }
 
-        galleryLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+        ActivityResultLauncher<Intent> launchImagePicker = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                 result -> {
-                    if (result.getResultCode() == RESULT_OK) {
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
                         Intent data = result.getData();
                         if (data != null) {
-                            Uri imageUri = data.getData();
-                            if (imageUri != null) {
-                                if (isPerfilButtonPressed) {
-                                    exibirImagemSelecionada(imageUri);
-                                } else {
-                                    exibirImagemFundoSelecionada(imageUri);
+                            Uri selectedImageUri = data.getData();
+                            if (selectedImageUri != null) {
+                                // Exibir a imagem selecionada no ImageView com corte circular
+                                try {
+                                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                                    Glide.with(perfil_config.this)
+                                            .load(bitmap)
+                                            .circleCrop()
+                                            .into(imageView);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                });
+        ActivityResultLauncher<Intent> launchImagePickerFundo = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Uri selectedImageUri = data.getData();
+                            if (selectedImageUri != null) {
+                                try {
+                                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                                    Glide.with(perfil_config.this)
+                                            .load(bitmap)
+                                            .into(imgFundo);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
                                 }
                             }
                         }
@@ -155,16 +174,29 @@ public class perfil_config extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 isPerfilButtonPressed = true;
-                abrirGaleria();
+                // Criar um Intent para selecionar a imagem
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*"); // Apenas imagens
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                // Iniciar a atividade de seleção de imagem e obter o resultado
+                launchImagePicker.launch(intent);
             }
         });
+
 
 
         btnEnviarImgFundo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isPerfilButtonPressed = false;
-                abrirGaleriaFundo();
+                isFundoButtonPressed = true;
+                // Criar um Intent para selecionar a imagem
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*"); // Apenas imagens
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                // Iniciar a atividade de seleção de imagem e obter o resultado
+                launchImagePickerFundo.launch(intent);
             }
         });
 
@@ -172,51 +204,6 @@ public class perfil_config extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 boolean atualizacaoRealizada = false; // Variável para controlar se ocorreu alguma atualização
-
-                // Verificar se uma imagem de perfil foi selecionada
-                Drawable drawable = imageView.getDrawable();
-                Bitmap drawableBitmap = null;
-                if (drawable != null && drawable instanceof BitmapDrawable) {
-                    drawableBitmap = ((BitmapDrawable) drawable).getBitmap();
-                    if (drawableBitmap != null && drawableBitmap.getWidth() > 0 && drawableBitmap.getHeight() > 0) {
-                        // Converter o Bitmap em um array de bytes (JPEG com qualidade 100)
-                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                        drawableBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                        byte[] imageBytes = stream.toByteArray();
-                        try (PreparedStatement statement = con.prepareStatement("UPDATE tblUsuario SET icon_usuario = ? WHERE cod_usuario = ?")) {
-                            statement.setBytes(1, imageBytes);
-                            statement.setInt(2, codigoUsuario);
-                            statement.executeUpdate();
-                            atualizacaoRealizada = true; // Atualização realizada
-                        } catch (SQLException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-
-
-
-                // Verificar se uma imagem de fundo foi selecionada
-                Drawable fundoDrawable = imgFundo.getDrawable();
-                Bitmap fundoBitmap = null;
-                if (fundoDrawable != null && fundoDrawable instanceof BitmapDrawable) {
-                    fundoBitmap = ((BitmapDrawable) fundoDrawable).getBitmap();
-                    if (fundoBitmap != null) {
-                        // Converter o Bitmap em um array de bytes (JPEG com qualidade 100)
-                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                        fundoBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                        byte[] imageBytes = stream.toByteArray();
-                        try (PreparedStatement statement = con.prepareStatement("UPDATE tblUsuario SET imgfundo_usuario = ? WHERE cod_usuario = ?")) {
-                            statement.setBytes(1, imageBytes);
-                            statement.setInt(2, codigoUsuario);
-                            statement.executeUpdate();
-                            atualizacaoRealizada = true; // Atualização realizada
-                        } catch (SQLException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-
 
                 // Verificar se o usuário inseriu algum nome
                 String nome = txtNome.getText().toString();
@@ -243,6 +230,34 @@ public class perfil_config extends AppCompatActivity {
                         a.printStackTrace();
                     }
                 }
+                // Enviar a imagem selecionada para o banco de dados
+                if (isPerfilButtonPressed) { // Verificar se o botão de perfil foi pressionado
+                    Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                    byte[] imagemPerfil = getBytesFromBitmap(bitmap, Bitmap.CompressFormat.JPEG);
+
+                    try (PreparedStatement statement = con.prepareStatement("UPDATE tblUsuario SET icon_usuario = ? WHERE cod_usuario = ?")) {
+                        statement.setBytes(1, imagemPerfil);
+                        statement.setInt(2, codigoUsuario);
+                        statement.executeUpdate();
+                        atualizacaoRealizada = true; // Atualização realizada
+                    } catch (SQLException a) {
+                        a.printStackTrace();
+                    }
+                }
+
+                if(isFundoButtonPressed){
+                    Bitmap bitmap = ((BitmapDrawable) imgFundo.getDrawable()).getBitmap();
+                    byte[] imagemFundo = getBytesFromBitmap(bitmap, Bitmap.CompressFormat.JPEG);
+                    try (PreparedStatement statement = con.prepareStatement("UPDATE tblUsuario SET imgFundo_usuario = ? WHERE cod_usuario = ?")) {
+                        statement.setBytes(1, imagemFundo);
+                        statement.setInt(2, codigoUsuario);
+                        statement.executeUpdate();
+                        atualizacaoRealizada = true; // Atualização realizada
+                    } catch (SQLException a) {
+                        a.printStackTrace();
+                    }
+                }
+
 
                 // Exibir o Toast apenas se ocorreu alguma atualização
                 if (atualizacaoRealizada) {
@@ -250,73 +265,14 @@ public class perfil_config extends AppCompatActivity {
                 }
             }
         });
+
     }
 
-    private void abrirGaleriaFundo() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        galleryLauncher.launch(intent);
+    // Método para converter um Bitmap em um array de bytes usando o formato especificado
+    private byte[] getBytesFromBitmap(Bitmap bitmap, Bitmap.CompressFormat format) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(format, 100, stream);
+        return stream.toByteArray();
     }
-
-    private void exibirImagemFundoSelecionada(Uri imageUri) {
-        try {
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-            imgFundo.setImageBitmap(bitmap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void abrirGaleria() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        galleryLauncher.launch(intent);
-    }
-
-    private void exibirImagemSelecionada(Uri imageUri) {
-        try {
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-            Bitmap roundedBitmap = getRoundedBitmap(bitmap);
-            imageView.setImageBitmap(roundedBitmap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Bitmap getRoundedBitmap(Bitmap bitmap) {
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-        int radius = Math.min(width, height) / 2;
-
-        Bitmap croppedBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(croppedBitmap);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setShader(new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
-        canvas.drawCircle(width / 2, height / 2, radius, paint);
-
-        return croppedBitmap;
-    }
-
-    private boolean verificarNomeExistente(String nome) {
-        Acessa objA = new Acessa();
-        Connection con = objA.entBanco(this);
-        SharedPreferences sharedPreferences = this.getSharedPreferences("SessaoUsuario", Context.MODE_PRIVATE);
-        int codigoUsuario = sharedPreferences.getInt("codigoUsuario", -1);
-        boolean nomeExistente = false;
-
-        try (PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) AS count FROM tblUsuario WHERE cod_usuario != ? AND nome = ?")) {
-            statement.setInt(1, codigoUsuario);
-            statement.setString(2, nome);
-            ResultSet rs = statement.executeQuery();
-            if (rs.next()) {
-                int count = rs.getInt("count");
-                nomeExistente = (count > 0);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-        return nomeExistente;
-    }
-
 
 }
