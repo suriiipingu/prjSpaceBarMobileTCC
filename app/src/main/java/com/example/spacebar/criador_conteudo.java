a8da5c73f4b15f7cee84038fd7ce0596cc75047b
Clara Sanson <btsherwatching@gmail.com>
2023-06-14 00:28:23 -0300

    fim do design 2

--- app/src/main/java/com/example/spacebar/criador_conteudo.java
+++ app/src/main/java/com/example/spacebar/criador_conteudo.java
@@ -2,11 +2,15 @@ package com.example.spacebar;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.Connection;
@@ -23,6 +27,21 @@ public class criador_conteudo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criador_conteudo);
 
+        int statusBarColor = getResources().getColor(R.color.white);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(statusBarColor);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(statusBarColor);
+        }
+
+        //tirar a barra de titulo da pagina
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         LinearLayout inserirLinearLayout = findViewById(R.id.inserirLinearLayout);
 
         // obtendo a Conexao
